3b2444749360f61675f052088ccbd120c932d68d
Ada4489 <6645andy@gmail.com>
2020-10-15 00:30:20 +0900

    Kernel

---
+++ linux-5.4.67/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP Tue Oct 13 18:21:14 KST 2020"
+#define LINUX_COMPILE_BY "donghoon"
+#define LINUX_COMPILE_HOST "Ada"
+#define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04)"
